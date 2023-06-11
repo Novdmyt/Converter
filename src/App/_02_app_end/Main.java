3ff4bb54a553e9717aaa0efe2ea3289caf69f71c
Дмитрий <mitrofan198522@gmail.com>
2023-06-11 12:31:29 +0300

    Add version

--- src/App/_02_app_end/Main.java
+++ src/App/_02_app_end/Main.java
@@ -1,4 +1,8 @@
 package App._02_app_end;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Converter for base converting");
+        System.out.println("Version 1.0");
+    }
 }
