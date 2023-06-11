4cc2da7c3397df283ab870f5012bff8b7d81da47
Дмитрий <mitrofan198522@gmail.com>
2023-06-11 12:42:34 +0300

    Add variable

--- src/App/_02_app_end/Main.java
+++ src/App/_02_app_end/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Converter for base converting");
         System.out.println("Version 1.0");
+        double mille = 55.3;
+        double kilometer = 77.3;
     }
 }
